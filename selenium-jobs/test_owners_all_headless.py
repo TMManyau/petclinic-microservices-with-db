f929e2220edf70516df0501361f92383ab5f3098
Tinotenda Martin <tmmanyau@gmail.com>
2022-10-14 01:14:27 +0000

    updated value.yaml, values-template.yml, test_owners_all_headless.py files

--- selenium-jobs/test_owners_all_headless.py
+++ selenium-jobs/test_owners_all_headless.py
@@ -1,17 +1,10 @@
 from selenium import webdriver
-<<<<<<< HEAD
-from selenium.webdriver.common.keys import Keys
-from time import sleep
-import random
-import os
-=======
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import os
 
->>>>>>> feature/msp-18
 # Set chrome options for working with headless mode (no screen)
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("headless")
@@ -20,49 +13,6 @@ chrome_options.add_argument("disable-dev-shm-usage")
 
 # Update webdriver instance of chrome-driver with adding chrome options
 driver = webdriver.Chrome(options=chrome_options)
-<<<<<<< HEAD
-
-# Connect to the application
-APP_IP = os.environ['MASTER_PUBLIC_IP']
-url = "http://"+APP_IP.strip()+":30001/"
-print(url)
-driver.get(url)
-owners_link = driver.find_element_by_link_text("OWNERS")
-owners_link.click()
-sleep(2)
-all_link = driver.find_element_by_link_text("REGISTER")
-all_link.click()
-sleep(2)
-# Register new Owner to Petclinic App
-fn_field = driver.find_element_by_name('firstName')
-fn = 'Callahan' + str(random.randint(0, 100))
-fn_field.send_keys(fn)
-sleep(1)
-fn_field = driver.find_element_by_name('lastName')
-fn_field.send_keys('Clarusway')
-sleep(1)
-fn_field = driver.find_element_by_name('address')
-fn_field.send_keys('Ridge Corp. Street')
-sleep(1)
-fn_field = driver.find_element_by_name('city')
-fn_field.send_keys('McLean')
-sleep(1)
-fn_field = driver.find_element_by_name('telephone')
-fn_field.send_keys('+1230576803')
-sleep(1)
-fn_field.send_keys(Keys.ENTER)
-
-# Wait 10 seconds to get updated Owner List
-sleep(10)
-# Verify that new user is added to Owner List
-if fn in driver.page_source:
-    print(fn, 'is added and found in the Owners Table')
-    print("Test Passed")
-else:
-    print(fn, 'is not found in the Owners Table')
-    print("Test Failed")
-driver.quit()
-=======
 # driver = webdriver.Chrome("/Users/home/Desktop/chromedriver")
 # Connect to the application
 APP_IP = os.environ['MASTER_PUBLIC_IP']
@@ -85,4 +35,3 @@ verify_table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((B
 print("Table loaded")
 
 driver.quit()
->>>>>>> feature/msp-18
